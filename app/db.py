e029c7ca7f11d2d7bafedd8516cac614ac20489d
almog1020 <102291459+almog1020@users.noreply.github.com>
2026-03-08 21:06:34 +0200

    Modify database URL to prioritize environment variable
    Updated database connection URL to use environment variable.

--- app/db.py
+++ app/db.py
@@ -3,7 +3,6 @@ import os
 from sqlalchemy import Engine
 from sqlmodel import SQLModel
 
-# Use environment variable if available (for Docker), otherwise use localhost
 postgresql_url = os.getenv(
     "DATABASE_URL",
     default="postgresql://Almog:1999@127.0.0.1:5432/levelup"
